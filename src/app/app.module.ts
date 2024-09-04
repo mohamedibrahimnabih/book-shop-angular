475a45d4f7be000f19f14a50c4be08d669e3e07a
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-09-04 14:10:27 +0300

    Fix Component Folder Name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/header/header.component';
-import { BodyComponent } from './component/body/body.component';
-import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
